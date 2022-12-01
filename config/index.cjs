bae5c1979a59394f053aa5fcd430a4448283aafc
zywang1 <56786280+zywang1@users.noreply.github.com>
2022-12-01 21:39:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otfM-5vihCkt8T-Fik_wk16BllBM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4esZb7s55QVb_R8dUdoAIc5eJd-pdvVTXuhHFBZUMIg',
+      useTemplateId: 'G3tGs0Zp0wzZlIwmhnBHXfew8sHjM2542KX9hXiwN5U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-14',
       festivals: [
