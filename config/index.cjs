213220dbfaf4baf3f27f1b899d980a74f7eaf564
zywang1 <56786280+zywang1@users.noreply.github.com>
2022-12-02 20:15:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otfM-5r56_SS_UJXSoQHVGBzhuaA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Y-YIdfAx64lFQixoJUS-VAm3UsPyfCyVtW-VvwLEJWc',
+      useTemplateId: 'bDJsFmpaqLCSO1zggBioK2EBJo1zHYhLCMrlbChshKs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-14',
       festivals: [
@@ -32,9 +32,9 @@ const USER_CONFIG = {
           type: '生日', name: '小王', year: '1997', date: '01-10',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '小顾', year: '1997', date: '04-14',
-        },
+//         {
+//           type: '生日', name: '小顾', year: '1997', date: '04-14',
+//         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
