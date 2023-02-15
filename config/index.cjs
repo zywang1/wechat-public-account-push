59ac9a808719ea1a62bbd032b396065a0f96c22f
zywang1 <56786280+zywang1@users.noreply.github.com>
2023-02-15 22:08:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otfM-5r56_SS_UJXSoQHVGBzhuaA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1r34cWaEHpykchoViSj3XdgMWO0rKIvrJwtXN5jZBy8',
+      useTemplateId: 'C-wPKhTIUYlmXk0UbyafvtR-QKdGkWYbUNlKfIceN1g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-14',
       festivals: [
@@ -50,7 +50,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otfM-5vihCkt8T-Fik_wk16BllBM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1r34cWaEHpykchoViSj3XdgMWO0rKIvrJwtXN5jZBy8',
+      useTemplateId: 'C-wPKhTIUYlmXk0UbyafvtR-QKdGkWYbUNlKfIceN1g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-27',
       openUrl: 'https://wangxinleo.cn',
