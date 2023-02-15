75126b60d046c8200f735db4731450c422d08042
zywang1 <56786280+zywang1@users.noreply.github.com>
2023-02-15 22:04:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,40 +17,40 @@ const USER_CONFIG = {
   CITY: '上海',
 
   USERS: [
-    {
-      // 想要发送的人的名字
-      name: '亲爱的小顾',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'otfM-5r56_SS_UJXSoQHVGBzhuaA',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oiOU5tJ6hx3Y4jjdDCDfG0WXqM67z6k4R17iXtkAulM',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-14',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '小王', year: '1997', date: '12-19',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '小顾', year: '1997', date: '04-14',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-11-11' },
-        // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
+//     {
+//       // 想要发送的人的名字
+//       name: '亲爱的小顾',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'otfM-5r56_SS_UJXSoQHVGBzhuaA',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       useTemplateId: 'oiOU5tJ6hx3Y4jjdDCDfG0WXqM67z6k4R17iXtkAulM',
+//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       horoscopeDate: '04-14',
+//       festivals: [
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: '小王', year: '1997', date: '12-19',
+//         },
+//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '生日', name: '小顾', year: '1997', date: '04-14',
+//         },
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2022-11-11' },
+//         // 结婚纪念日
+//         //{ keyword: 'marry_day', date: '2022-09-09' },
+//       ],
+//     },
     {
       // 想要发送的人的名字
       name: '亲爱的小顾',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otfM-5vihCkt8T-Fik_wk16BllBM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oiOU5tJ6hx3Y4jjdDCDfG0WXqM67z6k4R17iXtkAulM',
+      useTemplateId: '1r34cWaEHpykchoViSj3XdgMWO0rKIvrJwtXN5jZBy8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-27',
       openUrl: 'https://wangxinleo.cn',
